a98084cdc9cc9c2c833cef19046f48a8988badd9
Ishi Agarwal <120496566+ishi-ag27@users.noreply.github.com>
2024-01-17 23:08:27 +0530

    Create 1207 Unique Number Of Occurences.cpp

---
+++ 1207 Unique Number Of Occurences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        //Code using hash tables:
+      /*  unordered_map<int,int>mp;
+        for(int x:arr){
+            mp[x]++;
+        }
+        unordered_set<int>st;
+
+        for(auto it:mp){
+            int freq=it.second;  //it.first=number
+
+            if(st.find(freq)!=st.end()){
+                return false;
+            }
+            st.insert(freq);
+
+        }
+        return true;
+    }*/
+    //Code using arrays 
+    vector<int>vec(2001,0);
+    for(int x:arr){
+        vec[x+1000]++;
+
+    }
+    sort(begin(vec),end(vec));
+    for(int i=1;i<2001;i++){
+        if(vec[i]!=0 && vec[i]==vec[i-1]){
+            return false;
+        }
+    }
+    return true;
+    }
+    
+};
