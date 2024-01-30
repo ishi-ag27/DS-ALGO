f2bb6ebf9b38e28a321d370ba6e373ee083b5eb0
Ishi Agarwal <120496566+ishi-ag27@users.noreply.github.com>
2024-01-31 00:10:59 +0530

    Update 150. Evaluate Reverse Polish Notation.cpp

--- 150. Evaluate Reverse Polish Notation.cpp
+++ 150. Evaluate Reverse Polish Notation.cpp
@@ -31,6 +31,39 @@ public:
            st.push(stoi(token));
        }
     }
+
     return st.top();
     }
 };
+//Approach 2 Using Unordered_map and Lambda
+/*class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        unordered_map<string,function<int (int,int)>>mp={
+            {"+",[](int a,int b){return a+b;}},
+            {"-",[](int a,int b){return a-b;}},
+            {"*",[](int a,int b){return (long)a*(long)b;}},
+            {"/",[](int a,int b){return a/b;}}
+        };
+
+    for(string &token : tokens){
+       if(token=="+"||token=="-"||token=="*"||token=="/"){
+           //top 2 elements ko pop karke operate karlo
+           //then push the result in stack
+            int b=st.top();
+            st.pop();
+            int a =st.top();
+            st.pop();
+            int result =mp[token](a,b);
+            st.push(result);
+       } 
+       else{
+           st.push(stoi(token));
+       }
+    }
+    return st.top();
+    
+    }
+};
+*/
