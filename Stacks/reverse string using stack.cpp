7f799e83468dfe6197a53bc8fa9b46d7c1790432
Ishi Agarwal <120496566+ishi-ag27@users.noreply.github.com>
2024-05-09 20:30:51 +0530

    Create reverse string using stack.cpp

---
+++ Stacks/reverse string using stack.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        stack<char>s;
+        for(int i=0;i<str.length();i++){
+            char ch=str[i];
+            s.push(ch);
+        }
+        string ans="";
+        while(!s.empty()){
+            char ch=s.top();
+            ans.push_back(ch);
+            s.pop();
+        }
+    return ans;
+    }
+};
