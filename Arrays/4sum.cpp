18c298d7d0f071cc0214fd24cfdc13d285106e7d
Ishi Agarwal <120496566+ishi-ag27@users.noreply.github.com>
2024-07-09 21:26:00 +0530

    Create 4sum.cpp

---
+++ Arrays/4sum.cpp
@@ -0,0 +1,104 @@
+//Approach 1: Brute force
+//TC: O(n^4)
+//SC:O(no of quads*2)
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>>st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    for(int l=k+1;l<n;l++){
+                        long long sum=nums[i];
+                        sum +=nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+                        if(sum==target){
+                            vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                            sort(temp.begin(),temp.end());
+                            st.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+    }
+};
+
+//Approach 2: Hashing 
+//TC: O(n^3 * log(no of quads))
+//SC: O(N) //hashset +O(quads*2) //unique set & return array
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>>st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<int>hashset;
+                for(int k=j+1;k<n;k++){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    int fourth=target-sum;
+                    if (hashset.find(fourth)!=hashset.end()){
+                        vector<int>temp={nums[i],nums[j],nums[k],fourth};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                     hashset.insert(nums[k]);
+                }
+               
+            }
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+    }
+};
+//Approach 3: Two Pointer Technique 
+//TC: O(N^3)
+//SC: O(no. of quads)
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1])continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){
+                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1])k++;
+                        while(k<l && nums[l]==nums[l+1])l--;
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
